cd645e8991d3dd48080d11cb40eacc5e5ac9ebe8
Juan Ramos <juancagb.17@gmail.com>
2018-11-28 22:10:27 -0500

    Add device token to users (FCM)

---
+++ app/Http/Controllers/Api/FirebaseController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FirebaseController extends Controller
+{
+    public function postToken(Request $request)
+    {
+    	// $request->validate($rules);
+
+    	$user = Auth::guard('api')->user();
+    	
+    	if ($request->has('device_token')) {
+	    	$user->device_token = $request->input('device_token');
+	    	$user->save();	
+    	}    	
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 Route::post('/login', 'AuthController@login');
 Route::post('/register', 'AuthController@register');
 
-
 // Public resources
 Route::get('/specialties', 'SpecialtyController@index');
 Route::get('/specialties/{specialty}/doctors', 'SpecialtyController@doctors');
@@ -17,4 +16,7 @@ Route::middleware('auth:api')->group(function () {
 	// appointments
 	Route::get('/appointments', 'AppointmentController@index');
 	Route::post('/appointments', 'AppointmentController@store');
+
+	// fcm
+	Route::post('/fcm/token', 'FirebaseController@postToken');
 });
