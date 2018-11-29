47ac16fd9d6db1b6a32aa542cd21f2a3d588e9d5
Juan Ramos <juancagb.17@gmail.com>
2018-11-28 22:32:34 -0500

    Add use Auth in FirebaseController

--- app/Http/Controllers/Api/FirebaseController.php
+++ app/Http/Controllers/Api/FirebaseController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use Auth;
+
 class FirebaseController extends Controller
 {
     public function postToken(Request $request)
