41dc55aa88740de1aa45f1993dbe1c0e9fa74396
Juan Ramos <juancagb.17@gmail.com>
2018-11-29 16:20:51 -0500

    Fix Carbon object using copy()

--- app/Console/Commands/SendNotifications.php
+++ app/Console/Commands/SendNotifications.php
@@ -35,7 +35,7 @@ class SendNotifications extends Command
 
         $headers = ['id', 'scheduled_date', 'scheduled_time', 'patient_id'];
 
-        $appointmentsTomorrow = $this->getAppointments24Hours($now);
+        $appointmentsTomorrow = $this->getAppointments24Hours($now->copy());
 
         foreach ($appointmentsTomorrow as $appointment) {
             $appointment->patient->sendFCM('No olvides tu cita mañana a esta hora.');
@@ -44,7 +44,7 @@ class SendNotifications extends Command
 
         $this->table($headers, $appointmentsTomorrow);
 
-        $appointmentsNextHour = $this->getAppointmentsNextHour($now);
+        $appointmentsNextHour = $this->getAppointmentsNextHour($now->copy());
 
         foreach ($appointmentsNextHour as $appointment) {
             $appointment->patient->sendFCM('Tienes una cita en 1 hora. Te esperamos.');
